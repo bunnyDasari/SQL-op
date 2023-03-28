2e0640420fbf8e60f0115a635038a53eadce094e
Dasari Rohan <104191071+bunnyDasari@users.noreply.github.com>
2023-03-28 22:18:29 +0530

    Add files via upload

---
+++ SQL CODE PLAYGROUND.sql
@@ -0,0 +1,39 @@
+INSERT INTO
+  player(name, age, score)
+VALUES
+  ("Rohan", 19, 100),
+  ("John", 15, 200);
+
+SELECT
+  name,
+  age,
+  score
+FROM
+  player;
+
+SELECT
+  *
+FROM
+  player
+SELECT
+  *
+FROM
+  player
+WHERE
+  score = 200
+UPDATE
+  player
+SET
+  age = 0
+WHERE
+  name = "Rohan"
+DELETE FROM
+  product
+DELETE FROM
+  player DROP TABLE player
+  /*deleting */
+ALTER TABLE
+  college
+ADD
+  name_table INT
+  /*creating the column*/
